ffe13b86287ef233aaf7cb1759783bb036554a2c
cobiii <glogovsekgal@gmail.com>
2018-04-16 09:19:31 +0200

    added Solving page and style to Home page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.precompile += %w( signin.css )
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+Rails.application.config.assets.precompile += %w( home.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
