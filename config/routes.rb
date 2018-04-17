9969e6bb14d72ecf473e855a65643ab9b8e4f79d
cobiii <glogovsekgal@gmail.com>
2018-04-17 08:29:06 +0200

    fixed editor

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  resources :problems do
-    get 'code_request'
-  end
+  resources :problems
   get 'home/index'
 
   devise_for :users
   root 'home#index'
-  get 'editor/:id' => 'home#editor', :as => :editor
+  get 'editor/:id/' => 'home#editor', :as => :editor
   get 'challanges/' => 'problems#index_home', :as => :solve
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
